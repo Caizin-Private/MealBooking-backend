8578a8d263c9b719c27ace952779ab27bb679695
mishradivyanshi <mdivyanshi563@gmail.com>
2026-01-19 11:40:53 +0530

    to select the most recent cutoff time

--- backend-smart-workplace/src/main/java/org/example/repository/CutoffConfigRepository.java
+++ backend-smart-workplace/src/main/java/org/example/repository/CutoffConfigRepository.java
@@ -3,5 +3,10 @@ package org.example.repository;
 import org.example.entity.CutoffConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CutoffConfigRepository extends JpaRepository<CutoffConfig, Long> {
+
+    Optional<CutoffConfig> findTopByOrderByIdDesc();
+
 }
