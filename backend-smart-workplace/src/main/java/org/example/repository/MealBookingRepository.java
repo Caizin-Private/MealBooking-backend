42747706824c05c9b88c574a54151b2b4bbb4c4e
mishradivyanshi <mdivyanshi563@gmail.com>
2026-01-19 14:00:13 +0530

    add DTO and repository support for meal cancellation

--- backend-smart-workplace/src/main/java/org/example/repository/MealBookingRepository.java
+++ backend-smart-workplace/src/main/java/org/example/repository/MealBookingRepository.java
@@ -5,8 +5,10 @@ import org.example.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public interface MealBookingRepository extends JpaRepository<MealBooking, Long> {
 
     boolean existsByUserAndBookingDate(User user, LocalDate bookingDate);
+    Optional<MealBooking> findByUserAndBookingDate(User user, LocalDate date);
 }
