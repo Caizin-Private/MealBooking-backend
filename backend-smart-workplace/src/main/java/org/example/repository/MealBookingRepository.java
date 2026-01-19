affc9290e0abfcdf02c9451f3671ae59283d32cb
mishradivyanshi <mdivyanshi563@gmail.com>
2026-01-19 11:38:45 +0530

    implement start-to-end date booking logic with validations

--- backend-smart-workplace/src/main/java/org/example/repository/MealBookingRepository.java
+++ backend-smart-workplace/src/main/java/org/example/repository/MealBookingRepository.java
@@ -1,12 +1,12 @@
 package org.example.repository;
 
 import org.example.entity.MealBooking;
+import org.example.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 
-public interface MealBookingRepository
-        extends JpaRepository<MealBooking, Long> {
+public interface MealBookingRepository extends JpaRepository<MealBooking, Long> {
 
-    boolean existsByUserIdAndBookingDate(Long userId, LocalDate bookingDate);
+    boolean existsByUserAndBookingDate(User user, LocalDate bookingDate);
 }
--- backend-smart-workplace/src/main/java/org/example/service/MealBookingService.java
+++ backend-smart-workplace/src/main/java/org/example/service/MealBookingService.java
@@ -3,13 +3,13 @@ package org.example.service;
 import org.example.entity.User;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public interface MealBookingService {
 
     void bookMeals(
             User user,
-            List<LocalDate> bookingDates,
+            LocalDate startDate,
+            LocalDate endDate,
             double latitude,
             double longitude
     );
