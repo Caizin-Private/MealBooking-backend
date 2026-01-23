f8acc18e6489801acaa04d1cbbe85bb481ebaa31
Rohan Lande <rohan.lande@caizin.com>
2026-01-23 12:49:43 +0530

    fix: cors-issue allow all origins

--- backend-smart-workplace/src/main/java/org/example/config/CorsConfig.java
+++ backend-smart-workplace/src/main/java/org/example/config/CorsConfig.java
@@ -14,9 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")   // 🔥 important change
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
