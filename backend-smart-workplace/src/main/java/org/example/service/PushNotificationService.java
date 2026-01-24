681df6ad3a24bfd6c96c0054abacd90ac5d37156
mishradivyanshi-caizin <mishra.divaynshi@caizin.com>
2026-01-25 01:45:09 +0530

    Update push notification service contract

--- backend-smart-workplace/src/main/java/org/example/service/PushNotificationService.java
+++ backend-smart-workplace/src/main/java/org/example/service/PushNotificationService.java
@@ -21,5 +21,8 @@ public interface PushNotificationService {
 
     void sendLunchDefaultedNotification(Long userId, LocalDate date);
 
+    void sendLunchAvailableNotification(Long userId, LocalDate date);
+
+
 
 }
